daf52268b681b482aa968ec70105a5c8e8414aaa
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-09-13 17:47:17 -0300

    feat: add custom errors

---
+++ src/utils/error.ts
@@ -0,0 +1,10 @@
+import { toolkit } from "./toolkit";
+
+export const {
+	UnauthorizedError,
+	ForbiddenError,
+	MissingParamError,
+	InvalidParamError,
+	EmailInvalidError,
+	PasswordInvalidError,
+} = toolkit.error;
