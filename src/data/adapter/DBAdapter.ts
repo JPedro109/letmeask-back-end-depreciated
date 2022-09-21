579a49a04bdc57ff16146a6e063e1c191ad201cf
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-09-21 19:28:04 -0300

    refactor: modify methods in repository adapter

--- src/data/adapter/DBAdapter.ts
+++ src/data/adapter/DBAdapter.ts
@@ -15,12 +15,91 @@ export class DBAdapter<Type> implements IDBAdapter<Type> {
 		return await this.prisma[this.table].create({ data });
 	}
 
-	async getOne(data: object): Promise<Type> {
-		return await this.prisma[this.table].findUnique({ where: data });
+	async getOne(where: object, operator?: "AND" | "OR"): Promise<Type> {
+
+		if (Object.keys(where).length > 1) {
+
+			if (!operator) throw new Error("Você precisa colocar o operador da operação");
+
+			let whereModified = {};
+
+			for (const key in where) {
+				whereModified = {
+					...whereModified,
+					[key]: { equals: where[`${key}`] }
+				};
+			}
+
+			if (operator === "AND") {
+
+				const object = await this.prisma[this.table].findMany({
+					where: {
+						AND: whereModified
+					}
+				});
+
+				return object?.[0];
+			}
+
+			if (operator === "OR") {
+				const object = await this.prisma[this.table].findMany({
+					where: {
+						OR: whereModified
+					}
+				});
+
+				return object?.[0];
+			}
+
+		}
+
+		const object = await this.prisma[this.table].findMany({ where: { ...where } });
+
+		return object?.[0];
 	}
 
-	async getAll(where?: object): Promise<Type[]> {
-		return await this.prisma[this.table].findMany({ where: where || {} });
+	async getAll(where?: object, operator?: "AND" | "OR"): Promise<Type[]> {
+
+		if (Object.keys(where).length > 1) {
+
+			if (!operator) throw new Error("Você precisa colocar o operador da operação");
+
+			let whereModified = {};
+
+			for (const key in where) {
+				whereModified = {
+					...whereModified,
+					[key]: { equals: where[`${key}`] }
+				};
+			}
+
+			if (operator === "AND") {
+
+				const object = await this.prisma[this.table].findMany({
+					where: {
+						AND: whereModified
+					}
+				});
+
+				return object;
+			}
+
+			if (operator === "OR") {
+				const object = await this.prisma[this.table].findMany({
+					where: {
+						OR: whereModified
+					}
+				});
+
+				return object;
+			}
+
+		}
+
+		const object = await this.prisma[this.table].findMany({ where: { ...where || {} } });
+
+		return object;
+
 	}
 
 	async update(where: object, data: object): Promise<Type> {
--- src/data/adapter/IDBAdapter.ts
+++ src/data/adapter/IDBAdapter.ts
@@ -1,7 +1,7 @@
 export interface IDBAdapter<Type> {
 	insert(data: Type): Promise<Type>;
-	getOne(data: object): Promise<Type>;
-	getAll(where?: object): Promise<Type[]>;
+	getOne(where: object, operator?: "AND" | "OR"): Promise<Type>;
+	getAll(where?: object, operator?: "AND" | "OR"): Promise<Type[]>;
 	update(where: object, data: object): Promise<Type>;
 	delete(where: object): Promise<Type>;
 	deleteMany(where?: object): Promise<Type>;
