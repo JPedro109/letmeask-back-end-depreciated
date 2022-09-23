148b07a161b5dee72c8468e8c99ab565e2d7a781
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-09-23 20:25:17 -0300

    feat: router adapter

---
+++ src/core/adapter/adapterRouter/index.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { IRequestRouters } from "../interfaces/IRequestRouters";
+import { IResponse } from "../interfaces/IResponse";
+
+export const adapterRouter = (router: (request: IRequestRouters) => Promise<IResponse>) => {
+
+	return async (req: Request, res: Response) => {
+
+		try {
+			const { response, statusCode } = await router({
+				body: req.body,
+				query: req.query,
+				params: req.params,
+				userId: req.userId
+			});
+
+			return res.status(statusCode).json({ response });
+		} catch (e) {
+			const message = e?.message || "Internal Server Error";
+			const statusCode = e?.statusCode || 500;
+
+			console.error(message);
+			return res.status(statusCode).json({ message });
+		}
+	};
+};
---
+++ src/core/adapter/interfaces/IRequestRouters.ts
@@ -0,0 +1,9 @@
+export interface IRequestRouters {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	body: any;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	query: any;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	params: any;
+	userId: string;
+}
