d76934fe8cf0d7cac41f1a4d8aa31d8bafcdfcb8
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-06 19:45:00 -0300

    refactor: add new environment variable and update example

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { toolkit } from "./utils/toolkit";
 
 export const APP_URL = toolkit.environment.getRequiredValue("APP_URL");
+export const API_URL = toolkit.environment.getRequiredValue("API_URL");
 export const EMAIL_ORIGIN = toolkit.environment.getRequiredValue("EMAIL_ORIGIN");
 export const SECRET_KEY_JWT = toolkit.environment.getRequiredValue("SECRET_KEY_JWT");
 export const TOKEN_EXPIRY_TIME = toolkit.environment.getRequiredValue("TOKEN_EXPIRY_TIME");
