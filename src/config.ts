e057c716f9e70e187356067f8efdc53d1b9c98de
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-28 17:30:36 -0300

    refactor: remove redundancy

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@ export const ENVIRONMENT = toolkit.environment.getValue("ENVIRONMENT");
 export const APP_URL = toolkit.environment.getRequiredValue("APP_URL");
 export const API_URL = toolkit.environment.getRequiredValue("API_URL");
 export const TOKEN_EXPIRY_TIME = toolkit.environment.getRequiredValue("TOKEN_EXPIRY_TIME");
-export const PORT = parseInt(toolkit.environment.getRequiredValue("PORT")) || parseInt(process.env.PORT);
+export const PORT = parseInt(toolkit.environment.getRequiredValue("PORT"));
