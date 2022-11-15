19796abba39d3f2cb49e3e4d2d278f2363a0716b
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-15 16:05:24 -0300

    refactor: enable use case of delete room of delete the user question cache

--- src/core/useCases/Room/DeleteRoom/Rules.ts
+++ src/core/useCases/Room/DeleteRoom/Rules.ts
@@ -1,5 +1,7 @@
+import { Questions } from "../../../types/index";
 import { IRoomRepository } from "../../../../data/repositories/RoomRepository/IRoomRepository";
 import { UnauthorizedError } from "../../../../utils/error";
+import { toolkit } from "../../../../utils/toolkit";
 import { DTO } from "./DTO";
 
 export class Rules {
@@ -12,7 +14,22 @@ export class Rules {
 
 		if (roomCode !== roomCodeUser) throw new UnauthorizedError("Só o adminstrador da sala pode fazer essa ação");
 
+		const questions = toolkit.cache.get<Questions>(roomCode);
+		const questionsIds = questions?.map(element => element.id);
+
+		const userIds = questions?.map(element => element.userId);
+
+		userIds?.forEach(element => {
+			const userQuestions = toolkit.cache.get<Questions>(element);
+
+			const userQuestionsFilter = userQuestions.filter(element => !questionsIds.includes(element.id));
+
+			toolkit.cache.del(element);
+			toolkit.cache.set<Questions>(element, userQuestionsFilter);
+		});
+
 		await this.repository.destroy(roomCode);
+
 		return roomCode;
 	}
 }
